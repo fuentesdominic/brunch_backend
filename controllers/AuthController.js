5c1280060c79c823ec8a503a6c11aba8630b7cdb
Dominic Fuentes <militaryforce1@yahoo.com>
2023-03-24 14:15:22 -0500

    CheckSession function done

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -41,8 +41,14 @@ const Register = async (req, res) => {
       res.status(401).send({ status: "Error", msg: "An error has occurred!" });
     }
   };
+
+  const CheckSession = async (req, res) => {
+    const { payload } = res.locals;
+    res.send(payload);
+  };
   
   module.exports = {
     Register,
     Login,
+    CheckSession,
   };
