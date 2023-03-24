cf7a12f874fd826ced34384917443445f021c5c1
Dominic Fuentes <militaryforce1@yahoo.com>
2023-03-24 15:58:36 -0500

    user route complete

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,3 +1,11 @@
 const router = require("express").Router();
+const controller = require('../controllers/GradeController');
+const middleware = require('../middleware');
+
+router.get(
+    '/',
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.GetAllUsers);
 
 module.exports = router;
