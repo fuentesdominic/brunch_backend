b39d227d4626e8e80bec0a8047e8184656443afc
Dominic Fuentes <militaryforce1@yahoo.com>
2023-03-24 13:17:08 -0500

    setting up structure for routes to work

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
+const AuthRouter = require('./routes/AuthRouter');
+const MenuRouter = require('./routes/MenuRouter');
+const RestaurantRouter = require('./routes/RestaurantRouter');
+const UserRouter = require('./routes/UserRouter');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/')
+
+app.listen(PORT, () => console.log(`Server Running On Port: ${PORT}`));
