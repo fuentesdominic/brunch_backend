69c8dea7f7c8417bf60ed815f753df3c283d0a50
Dominic Fuentes <militaryforce1@yahoo.com>
2023-03-24 15:42:13 -0500

    made routes

--- routes/MenuRouter.js
+++ routes/MenuRouter.js
@@ -1,3 +1,35 @@
 const router = require("express").Router();
+const controller = require('../controllers/AuthController');
+const middleware = require('../middleware');
+
+router.get(
+    '/',
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.GetAllMenus);
+
+router.get(
+    '/:id', 
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.GetCourseById);
+    
+router.post(
+    '/create', 
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.CreateCourse);
+    
+router.put(
+    '/:id', 
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.UpdateCourseById);
+    
+router.delete(
+    '/:id', 
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.DeleteCourseById);
 
 module.exports = router;
