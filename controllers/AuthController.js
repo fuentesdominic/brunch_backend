4eb4d77e83c86afc44aefb4ec6013858f56bebb1
Dominic Fuentes <militaryforce1@yahoo.com>
2023-03-24 14:14:36 -0500

    login function done

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -12,7 +12,37 @@ const Register = async (req, res) => {
           throw err;
       }
   };
+
+  const Login = async (req, res) => {
+    try {
+      const user = await User.findOne({
+        where: { email: req.body.email },
+        raw: true,
+      });
+     
+      let matched = await middleware.comparePassword(
+        user.password,
+        req.body.password
+      );
+  
+      if (matched) {
+        let payload = {
+          id: user.id,
+          email: user.email,
+          name: user.name
+        };
+  
+        let token = middleware.createToken(payload);
+        return res.send({ user: payload, token });
+      }
+      res.status(401).send({ status: "Error", msg: "Unauthorized" });
+    } catch (error) {
+      console.log(error);
+      res.status(401).send({ status: "Error", msg: "An error has occurred!" });
+    }
+  };
   
   module.exports = {
     Register,
+    Login,
   };
