40b447ba1b85ecc4a97addcd7a0a979988aeb14e
Dominic Fuentes <militaryforce1@yahoo.com>
2023-03-27 09:50:06 -0500

    setting up user controller

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const logger = require("morgan");
 const AuthRouter = require('./routes/AuthRouter');
 const MenuRouter = require('./routes/MenuRouter');
 const RestaurantRouter = require('./routes/RestaurantRouter');
-// const UserRouter = require('./routes/UserRouter');
+const UserRouter = require('./routes/UserRouter');
 
 const app = express();
 
@@ -18,6 +18,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/auth', AuthRouter);
 app.use('/menu', MenuRouter);
 app.use('/restaurant', RestaurantRouter);
-// app.use('/user', UserRouter);
+app.use('/user', UserRouter);
 
 app.listen(PORT, () => console.log(`Server Running On Port: ${PORT}`));
