961a88e9412f598ad893d925b917cfc32bbf14c7
Dominic Fuentes <militaryforce1@yahoo.com>
2023-03-27 09:52:32 -0500

    get all users function

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,14 @@
 const { User } = require('../models');
 
+const GetAllUsers = async (req, res) => {
+	try {
+		const users = await User.findAll();
+		res.send(users);
+	} catch (err) {
+		throw err;
+	}
+};
+
 module.exports = {
-    
+    GetAllUsers,
 }
